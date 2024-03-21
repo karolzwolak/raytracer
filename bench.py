dbd4d0cef1fc90be6e4087066e7aeeeb8cdc3abb
Karol Zwolak <karolzwolak7@gmail.com>
2024-03-21 13:32:25 +0100

    feaf: improve bench script

--- bench.py
+++ bench.py
@@ -5,6 +5,7 @@ import subprocess
 BUILD_COMMAND = "cargo build -r"
 TARGET_DIR = "target/release/raytracer"
 RUN_COMMAND = f"{TARGET_DIR} "
+DEFAULT_TIMES = 2
 
 
 def build():
@@ -18,28 +19,32 @@ def run(args=""):
 def time_run(args=""):
     now = datetime.now()
     run(args)
+    print()
     elapsed = datetime.now() - now
     return elapsed.total_seconds()
 
 
 def run_n_times(n, args=""):
+    if n < 1:
+        return 0, 0
     time = 0
     for _ in range(n):
         time += time_run(args)
-    return time, time / n
+    return round(time, 3), round(time / n, 3)
 
 
 def main():
     build()
 
     args = sys.argv[1:]
-    times = int(args[0])
-    args = " ".join(args[1:]) if args else ""
-    time, avg = run_n_times(times, args)
 
-    print()
-    print(f"Total time: {time} seconds")
-    print(f"Average time: {avg} seconds")
+    times = int(args[0]) if args else DEFAULT_TIMES
+    run_args = " ".join(args[1:]) if args else ""
+    time, avg = run_n_times(times, run_args)
+
+    print(f"ran {times} times")
+    print(f"total time: {time} seconds")
+    print(f"average time: {avg} seconds")
 
 
 if __name__ == "__main__":
